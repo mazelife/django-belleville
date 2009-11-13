559044639f742a5fdd3ec944fc5cf3470e4fdc6a
mazelife <james.m.stevenson@gmail.com>
2009-11-13 02:43:34 +0000

    Initial import

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django=belleville',
+    version='0.1',
+    description='Yet another Django blogging tool',
+    author='James Stevenson',
+    author_email='james.m.stevenson@gmail.com',
+    url='http://code.google.com/p/django-belleville',
+    packages=['belleville'],
+    license = "Apache 2.0"
+)
