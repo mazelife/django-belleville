be7fe2abf231edaabd32ff2ffb3b6be9de135f42
mazelife <james.m.stevenson@gmail.com>
2009-12-30 19:28:40 +0000

    fixed error in urls.py that caused 500 errors to be raised when 404s should have been

--- belleville/urls.py
+++ belleville/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'site_index.index', name="index"),
+    url(r'^/$', 'site_index.index', name="index"),
     (r'^tumblelog/', include('blogging.tumblelog_urls', namespace="tumblelog")),
     (r'^blog/', include('blogging.blog_urls', namespace="blog")),
     (r'^authors/', include('authors.urls', namespace="authors")),
